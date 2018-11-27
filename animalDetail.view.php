c940f86fd22a8dc9343e4ac8050732ee82498884
xfrane16 <Jara.Franek2@gmail.com>
2018-11-27 18:23:26 +0100

    base view and styles of animalDetail

--- animalDetail.view.php
+++ animalDetail.view.php
@@ -10,9 +10,26 @@
 <html lang="cz">
   <head>
     <?php require_once("baseHeader.php");?>
+    <link rel="stylesheet" type="text/css" href="Styles/aniStyle.css">
+    <script src="scripts/jQuery.js"></script>
+    <script src="scripts/baseScripts.js"></script>
   </head>
   <body>
     <?php include 'menu.php';?>
-    Animal Detail
+    <h1><?php $actVM->animal->getColumnStringValue('ani_name') ?></h1>
+    <div class="anim_detail">
+      <p>Jmémo majitele: <?php $actVM->animal->getColumnStringValue('ani_owner') ?></p>
+      <p>Druh: <?php $actVM->animal->getColumnStringValue('ani_species') ?></p>
+      <p>Pohlaví: <?php $actVM->animal->getColumnStringValue('ani_sex') ?></p>
+      <p>Váha: <?php $actVM->animal->getColumnStringValue('ani_weight') ?></p>
+      <p>Stav: <?php $actVM->animal->getColumnStringValue('ani_state') ?></p>
+      <p>Datum narození: <?php $actVM->animal->getColumnStringValue('ani_birthday') ?></p>
+    </div>
+    <div class="swap_buttons">
+      <input type="submit" name="submit_ch" value="Léčby" class="swap_button" onclick="swapTables(1)"/>
+      <input type="submit" name="submit_ch" value="Vyšetření" class="swap_button" onclick="swapTables(2)"/>
+    </div>
+    <div class="chosen_detail">
+    </div>
   </body>
 </html>
