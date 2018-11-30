79e06b89174f88be14461ec8b14b62e2c8f273dd
xfrane16 <Jara.Franek2@gmail.com>
2018-11-30 13:03:04 +0100

    edit

--- ownerEdit.view.php
+++ ownerEdit.view.php
@@ -11,7 +11,7 @@
 <html lang="cz">
   <head>
     <?php require_once("baseHeader.php");?>
-    <link rel="stylesheet" type="text/css" href="Styles/ownStyle.css">
+    <link rel="stylesheet" type="text/css" href="Styles/editStyles.css">
     <script src="scripts/jQuery.js"></script>
     <script src="scripts/baseScripts.js"></script>
   </head>
@@ -20,31 +20,51 @@
     <div class="content">
       <form action="" method="post">
         <h1><?= $actVM->Name ?></h1>
-        <div class="ownm_detail">
-          <p>Jméno: <input type="text" value="<?= $actVM->Firstname ?>" name="own_name">
-            <?= (isset($actVM->Errors['own_name'])) ? $actVM->Errors['own_name'] : '' ?>
-          </p>
-          <p>Příjmení: <input type="text" value="<?= $actVM->Surname ?>" name="own_surname">
-            <?= (isset($actVM->Errors['own_surname'])) ? $actVM->Errors['own_surname'] : '' ?>
-          </p>
-          <p>Adresa: <input type="text" value="<?= $actVM->Address ?>" name="own_address">
-            <?= (isset($actVM->Errors['own_address'])) ? $actVM->Errors['own_address'] : '' ?>
-          </p>
-          <p>Pohlaví:
-            <select type="text" name="own_sex">
-              <?php foreach($actVM->SexSelect as $key => $value) {?>
-              <option value="<?= $key ?>"
-                      <?php if ($actVM->Sex == $value) echo "selected" ?> >
-                <?= $value ?>
-              </option>
-              <?php } ?>
-            </select>
-            <?= (isset($actVM->Errors['own_sex'])) ? $actVM->Errors['own_sex'] : '' ?>
-          </p>
-          <p>Telefoní číslo: <input type="text" value="<?= $actVM->Number ?>" name="own_mobile_number">
-            <?= (isset($actVM->Errors['own_mobile_number'])) ? $actVM->Errors['own_mobile_number'] : '' ?>
-          </p>
-        </div>
+        <div class="block">
+          <table class="edit_table">
+            <tr>
+              <th>Jméno:</th>
+              <td>
+                <input type="text" value="<?= $actVM->Firstname ?>" name="own_name">
+                <?= (isset($actVM->Errors['own_name'])) ? $actVM->Errors['own_name'] : '' ?>
+              </td>
+            </tr>
+            <tr>
+              <th>Příjmení:</th>
+              <td>
+                <input type="text" value="<?= $actVM->Surname ?>" name="own_surname">
+                <?= (isset($actVM->Errors['own_surname'])) ? $actVM->Errors['own_surname'] : '' ?>
+              </td>
+            </tr>
+            <tr>
+              <th>Adresa:</th>
+              <td>
+                <input type="text" value="<?= $actVM->Address ?>" name="own_address">
+                <?= (isset($actVM->Errors['own_address'])) ? $actVM->Errors['own_address'] : '' ?>
+              </td>
+            </tr>
+            <tr>
+              <th>Pohlaví:</th>
+              <td>
+                <select type="text" name="own_sex">
+                <?php foreach($actVM->SexSelect as $key => $value) {?>
+                <option value="<?= $key ?>"
+                        <?php if ($actVM->Sex == $value) echo "selected" ?> >
+                  <?= $value ?>
+                </option>
+                <?php } ?>
+                </select>
+                <?= (isset($actVM->Errors['own_sex'])) ? $actVM->Errors['own_sex'] : '' ?>
+              </td>
+            </tr>
+            <tr>
+              <th>Telefoní číslo:</th>
+              <td>
+                <input type="text" value="<?= $actVM->Number ?>" name="own_mobile_number">
+                <?= (isset($actVM->Errors['own_mobile_number'])) ? $actVM->Errors['own_mobile_number'] : '' ?>
+              </td>
+            </tr>
+          </table>
           <input type="submit" name="post_submit" value="Uložit" class="swap_button" />
       </form>
     </div>
