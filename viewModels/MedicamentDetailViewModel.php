81f70de4150bf82fd1fa2e35a71d3339457f8f20
Petr Fusek <xfusek08@stud.fut.vutbr.cz>
2018-11-30 22:01:38 +0100

    loding medicaments on species

--- viewModels/MedicamentDetailViewModel.php
+++ viewModels/MedicamentDetailViewModel.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once("lib/Logs.php");
 require_once("lib/DBEntityBrowser.php");
 
 require_once("DBEntities/MedicamentEntity.php");
@@ -22,8 +23,21 @@ class MedicamentDetailViewModel extends EditableDetailViewModelBase {
   public $TypeSelect = array();
   public $SpeciesSelect = array();
 
+  private $MesOnSpeciesBrwoser = null; // instance of DBEntityBrowser
+
   public function __construct() {
     parent::__construct('MedicamentEntity');
+  }
+
+  public function loadGetData() {
+    parent::loadGetData();
+    $this->MesOnSpeciesBrwoser = new DBEntityBrowser(
+      "MedicamentForSpeciesEntity",
+      "mfs_medpk = ?",
+      "spe_name"
+    );
+    $this->MesOnSpeciesBrwoser->addParams($this->Pk);
+    $this->MesOnSpeciesBrwoser->openBrowser();
   }
 
   public function initView() {
@@ -45,15 +59,8 @@ class MedicamentDetailViewModel extends EditableDetailViewModelBase {
     $this->Producer   = $this->MainDBEntity->getColumnStringValue('med_producer');
     $this->Substance  = $this->MainDBEntity->getColumnStringValue('med_active_substance');
 
-    $mesOnSpeciesBrwoser = new DBEntityBrowser(
-      "MedicamentForSpeciesEntity",
-      "mfs_medpk = ?",
-      "spe_name"
-    );
-    $mesOnSpeciesBrwoser->addParams($this->Pk);
-    $mesOnSpeciesBrwoser->openBrowser();
-
-    while (($actEntity = $mesOnSpeciesBrwoser->getNext()) != null) {
+    $this->initMedOnSpeBrowser();
+    while (($actEntity = $this->MesOnSpeciesBrwoser->getNext()) != null) {
       $mesOnSpecModel = new MedicamentForSpeciesModel();
       $mesOnSpecModel->Pk                = $actEntity->getColumnByName('mfs_pk')->getValue();;
       $mesOnSpecModel->MedPk             = $actEntity->getColumnByName('mfs_medpk')->getValue();;
@@ -65,7 +72,70 @@ class MedicamentDetailViewModel extends EditableDetailViewModelBase {
     }
   }
 
+  public function processPost() {
+    $this->loadGetData();
+    $this->MainDBEntity->loadFromPostData();
+    $isAllSuccess = $this->MainDBEntity->isDataValid();
+    $this->Errors = array_merge($this->Errors, $this->MainDBEntity->GetInvalidData($prefix));
+
+    $toDeletePKs = array();
+    $toSaveEntities = array();
+
+    $cnt = intval($_POST['medCount']);
+    while (($actEntity = $this->MesOnSpeciesBrwoser->getNext()) != null) {
+      $toDeletePKs = $actEntity->PK;
+    }
+
+    $index = 0;
+    while ($index < $cnt) {
+      $prefix = '';
+      if ($index == 0)
+        $prefix = $index . '_';
+
+      $actEntity = new MedicamentForSpeciesModel(intval($_POST[$prefix . 'mfs_pk']));
+
+      $indexOfPK = array_search($actEntity->Pk, $toDeletePKs);
+      if ($indexOfPK !== false)
+        unset($toDeletePKs[$indexOfPK]);
+
+      $actEntity->loadFromPostData($prefix);
+      $isAllSuccess = $isAllSuccess && $actEntity->isDataValid();
+      $this->Errors = array_merge($this->Errors, $actEntity->GetInvalidData($prefix));
+      $toSaveEntities[] = $actEntity;
+      $index = $index - 1;
+    }
+
+    if (!$isAllSuccess) {
+      $this->Message = STR_MSG_FORM_INVALID_DATA;
+      $this->initEdit();
+    } else {
+      $this->tryToSaveToDB($toSaveEntities, $toDeletePKs);
+    }
+  }
+
   public function onSuccessPost() {
     SessionControl::navigate("medicamentDetail.view?pk=" . $this->MainDBEntity->PK);
+  }
+
+  public function tryToSaveToDB($toSaveEntities, $toDeletePKs) {
+    try {
+      MyDatabase::$PDO->beginTransaction();
+      $success = true;
+
+      echo '<pre>', var_dumb($toSaveEntities), '</pre>';
+
+      if ($success) {
+        MyDatabase::$PDO->commit();
+      } else {
+        Logging::WriteLog(LogType::Anouncement, "RollBack");
+        MyDatabase::$PDO->rollBack();
+      }
+    } catch (PDOException $e) {
+      Log::WriteLog(LogType::Error, $e->getMessage());
+      Log::WriteLog(LogType::Anouncement, "RollBack");
+      MyDatabase::$PDO->rollBack();
+      $succes = false;
+    }
+    return $succes;
   }
 }
