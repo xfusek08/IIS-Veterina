85e9aeb1a8ca63bd4107cf885bd1c9e65de351cf
xfrane16 <Jara.Franek2@gmail.com>
2018-11-30 19:29:08 +0100

    fix

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
   <div class="menu">
     <ul class="menu_table">
       <li class="menu_choice"><a href="animalBrowse.view.php">Zvířata</a></li>
-      <li class="menu_choice"><a href="ownerBrowse.view.php">Majitelé</a></li>00
+      <li class="menu_choice"><a href="ownerBrowse.view.php">Majitelé</a></li>
       <li class="menu_choice"><a href="medicamentBrowse.view.php">Léky</a></li>    
       <?php if($isAdmin) { ?>
         <li class="menu_choice"><a href="employeeBrowse.view.php">Zaměstnanci</a></li>
