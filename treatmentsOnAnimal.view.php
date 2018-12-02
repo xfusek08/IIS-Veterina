ba9754ef1365f77335c63abe29b337de06e2850e
xfrane16 <Jara.Franek2@gmail.com>
2018-12-02 16:14:59 +0100

    style

--- treatmentsOnAnimal.view.php
+++ treatmentsOnAnimal.view.php
@@ -9,68 +9,49 @@
 ?>
 <div class="treatmentView">
   <h2><?= $actVM->Caption ?>-<?= $actVM->State ?></h2>
-  <div class="prognosis">Prognóza</div>
+  <h3>Prognóza</h3>
   <textarea readonly><?= $actVM->Prognosis ?></textarea>
-  <div class="flex">
-    <div class="flexPart">
-      <p>Spjatá vyšetření</p>
-      <table>
-        <thead>
-          <th>Datum</th>
-          <th>Typ</th>
-          <th>Hodina</th>
-          <th>Proběhlo</th>
-        </thead>
-        <tbody>
-          <?php foreach ($actVM->Examinations as $actExam) { ?>
-            <tr class="table_select" onclick="changePage(<?= $actExam->ExaPk ?>, 'examinationDetail.view.php')">
-              <td><a href="?pk=<?= $actExam->Pk ?>"><?= $actExam->Date ?></td>
-              <td><?= $actExam->Type ?></td>
-              <td><?= $actExam->Hour ?></td>
-              <td><?= $actExam->Ocurred ?></td>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
-    </div>
-    <div class="flexPart">
-    <p>Diagnózy</p>
-      <table>
-        <thead>
-          <th>Datum</th>
-          <th>Průběžná diagnóza</th>
-        </thead>
-        <tbody>
-          <?php foreach ($actVM->Examinations as $actExam) { ?>
-            <tr class="table_select" onclick="changePage(<?= $actExam->ExaPk ?>, 'examinationDetail.view.php')">
-            <?php if ($actExam->Ocurred == 'A') { ?>
-              <td><a href="?pk=<?= $actExam->Pk ?>"><?= $actExam->Date ?></td>
-              <td class="smallPad"><textarea readonly><?= $actExam->Diagnosis ?></textarea></td>
-            </tr>
-            <?php } ?>
-          <?php } ?>
-        </tbody>
-      </table>
-    </div>
-    <div class="flexPart">
-      <p>Předepsané léky</p>
-      <table>
-        <thead>
-          <th>Název</th>
-          <th>Délka užívání</th>
-          <th>Dávkování</th>
-        </thead>
-        <tbody>
-          <?php foreach ($actVM->Medicaments as $actMed) { ?>
-            <tr class="table_select" onclick="changePage(<?= $actMed->MedPk ?>, 'medicamentDetail.view.php')">
-              <td><a href="?pk=<?= $actMed->MedPk ?>"><?= $actMed->Name ?></a></td>
-              <td><?= $actMed->UsageTime ?></td>
-              <td><?= $actMed->Dosage ?></td>
-            </tr>
-          <?php } ?>
-        </tbody>
-      </table>
-    </div>
+  <div>
+    <h3>Spjatá vyšetření</h3>
+    <table>
+      <thead>
+        <th>Datum</th>
+        <th>Typ</th>
+        <th>Hodina</th>
+        <th>Proběhlo</th>
+        <th>Diagnóza</th>
+      </thead>
+      <tbody>
+        <?php foreach ($actVM->Examinations as $actExam) { ?>
+          <tr class="table_select" onclick="changePage(<?= $actExam->ExaPk ?>, 'examinationDetail.view.php')">
+            <td><a href="?pk=<?= $actExam->Pk ?>"><?= $actExam->Date ?></td>
+            <td><?= $actExam->Type ?></td>
+            <td><?= $actExam->Hour ?></td>
+            <td><?= $actExam->Ocurred ?></td>
+            <td class="smallPad"><textarea readonly><?= $actExam->Diagnosis ?></textarea></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+  <div>
+    <h3>Předepsané léky</h3>
+    <table>
+      <thead>
+        <th>Název</th>
+        <th>Délka užívání</th>
+        <th>Dávkování</th>
+      </thead>
+      <tbody>
+        <?php foreach ($actVM->Medicaments as $actMed) { ?>
+          <tr class="table_select" onclick="changePage(<?= $actMed->MedPk ?>, 'medicamentDetail.view.php')">
+            <td><a href="?pk=<?= $actMed->MedPk ?>"><?= $actMed->Name ?></a></td>
+            <td><?= $actMed->UsageTime ?></td>
+            <td><?= $actMed->Dosage ?></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
   </div>
 </div>
 <?php
